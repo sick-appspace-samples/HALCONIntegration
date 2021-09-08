14c274d8869a1cf50bdcc644ce769097d4647bf5
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:54:44 +0200

    'Update'

--- HALCONIntegration/scripts/main.lua
+++ HALCONIntegration/scripts/main.lua
@@ -1,28 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  HALCONIntegration
-
-  Summary:
-  Integration of HALCON procedures
-
-  Description:
-  This sample shows how HALCON procedures can be integrated into an Application.
-  There is a trivial HALCON procedure named "Binarize" which has been developed
-  using HDevelop and exported as a procedure. This procedure has been added to the
-  resources of this application. Have a look on the function 'binarizeImage' which
-  encapsulates the HALCON procedure call as well as the parameters of the procedure.
-
-  How to run:
-  The results can be seen in the 2D viewer on the webpage. Restarting the Sample may
-  be necessary to show images after loading the webpage.
-  To run this Sample a device with SICK Algorithm API, HALCON support and
-  AppEngine >= V2.5.0 is required. E.g. SIM4000 with latest firmware.
-
-  Implementation:
-  This sample is written using HALCON 12.
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 print('AppEngine Version: ' .. Engine.getVersion())
--- readme.md
+++ readme.md
@@ -1,16 +1,19 @@
 ## HALCONIntegration
 Integration of HALCON procedures
+
 ### Description
-This sample shows how HALCON procedures can be integrated into an Application.
+This sample shows how HALCON procedures can be integrated into an application.
 There is a trivial HALCON procedure named "Binarize" which has been developed
 using HDevelop and exported as a procedure. This procedure has been added to the
 resources of this application. Have a look on the function 'binarizeImage' which
 encapsulates the HALCON procedure call as well as the parameters of the procedure.
+
 ### How to Run
 The results can be seen in the 2D viewer on the web-page. Restarting the Sample may
 be necessary to show images after loading the web-page.
 To run this Sample a device with SICK Algorithm API, HALCON support and
 AppEngine >= V2.5.0 is required. E.g. SIM4000 with latest firmware.
+
 ### Implementation
 This sample is written using HALCON 12
 
